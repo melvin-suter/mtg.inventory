c5feb78bd8c308d73026f9a38b1c6ff15c7ebfe7
Melvin mngmt-02 <melvin@suter.dev>
2022-12-21 22:01:52 +0100

    build update

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+FRONTEND_VERSION=1.0.1
+BACKEND_VERSION=1.0.0
+
 cd mtg.inventory.backend
 
 dotnet publish --os linux --arch x64 -c Release -p:PublishProfile=DefaultContainer
 
-docker tag mtg_inventory_backend:1.0.0 suterdev/mtg.inventory.backend:1.0.0
-docker tag mtg_inventory_backend:1.0.0 suterdev/mtg.inventory.backend:latest
-docker push suterdev/mtg.inventory.backend:1.0.0
+docker tag mtg_inventory_backend:$BACKEND_VERSION suterdev/mtg.inventory.backend:$BACKEND_VERSION
+docker tag mtg_inventory_backend:$BACKEND_VERSION suterdev/mtg.inventory.backend:latest
+docker push suterdev/mtg.inventory.backend:$BACKEND_VERSION
 docker push suterdev/mtg.inventory.backend:latest
 
 
@@ -18,8 +21,8 @@ cd ../mtg.inventory.frontend
 npm i
 ng build
 
-docker build -t suterdev/mtg.inventory.frontend:1.0.0 .
-docker tag suterdev/mtg.inventory.frontend:1.0.0 suterdev/mtg.inventory.frontend:latest
-docker push suterdev/mtg.inventory.frontend:1.0.0
+docker build -t suterdev/mtg.inventory.frontend:$FRONTEND_VERSION .
+docker tag suterdev/mtg.inventory.frontend:$FRONTEND_VERSION suterdev/mtg.inventory.frontend:latest
+docker push suterdev/mtg.inventory.frontend:$FRONTEND_VERSION
 docker push suterdev/mtg.inventory.frontend:latest
 
